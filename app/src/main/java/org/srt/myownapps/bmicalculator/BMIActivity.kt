8ec505a21af3bf6f296d5179112156feada43c90
saber <sabertalukder@gmail.com>
2024-04-23 13:38:51 +0600

    ami abar shes

---
+++ app/src/main/java/org/srt/myownapps/bmicalculator/BMIActivity.kt
@@ -0,0 +1,11 @@
+package org.srt.myownapps.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BMIActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmiactivity)
+    }
+}
--- app/src/main/java/org/srt/myownapps/bmicalculator/MainActivity.kt
+++ app/src/main/java/org/srt/myownapps/bmicalculator/MainActivity.kt
@@ -1,11 +1,17 @@
 package org.srt.myownapps.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,BMIActivity::class.java))
+        },3000)
     }
 }
